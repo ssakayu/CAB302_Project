5fe1c986d35c117ce968ce12c152ec2c10f4cb44
Stanley <stanleycai111@gmail.com>
2026-03-19 14:09:42 +1000

    Merge remote master with local project

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hellooo Cab302 and welcome!");
+        System.out.printf("Hellooooooo Cab302 and welcome!");
+        SSystem.out.printf("Bye!!!! CAB302");
+
 
         for (int i = 1; i <= 5; i++) {
             git pull --rebase
