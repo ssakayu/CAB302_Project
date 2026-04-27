db8277b1211d8159421ed39b5b48259046e8a54c
Kawinpob Panbapeaw <Rungapa_jeamjit@hotmail.com>
2026-04-27 21:44:23 +1000

    Fix JavaFX project setup and main dashboard UI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import java.util.Objects; /*test*/
 
 public class Main extends Application {
 
